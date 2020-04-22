61a4191a62973e75c068ee5ffd6aacc7e10d2c45
sambowenhughes <sambowenhughes@hotmail.co.uk>
2020-04-22 19:31:25 +0100

    Redesigned app

---
+++ covid19-dashboard-swift/Pods/Target Support Files/Pods-covid19-dashboard-swift/Pods-covid19-dashboard-swift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_covid19_dashboard_swift : NSObject
+@end
+@implementation PodsDummy_Pods_covid19_dashboard_swift
+@end
--- covid19-dashboard-swift/covid19-dashboard-swift/Controller/ViewController.swift
+++ covid19-dashboard-swift/covid19-dashboard-swift/Controller/ViewController.swift
@@ -7,18 +7,20 @@
 //
 
 import UIKit
+import Charts
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, CovidServiceDelegate  {    
-    
-    @IBOutlet weak var deathTollLabel: UILabel!
-    @IBOutlet weak var countryLabel: UILabel!
+class ViewController: UIViewController  {
+    // Country Picker
     @IBOutlet weak var countryPicker: UIPickerView!
-    @IBOutlet weak var recoverRateLabel: UILabel!
-    @IBOutlet weak var numberOfCasesLabel: UILabel!
     
-    @IBOutlet weak var totalInfectedCount: UILabel!
-    @IBOutlet weak var totalRecoveredCount: UILabel!
-    @IBOutlet weak var totalDeathsCount: UILabel!
+    // Pie Chart Properties
+    @IBOutlet weak var pieChartView: PieChartView!
+    var totalDeaths = PieChartDataEntry(value:0)
+    var totalRecovered = PieChartDataEntry(value:0)
+    var totalInfected = PieChartDataEntry(value:0)
+    var numberOfDataEntries = [PieChartDataEntry]()
+    
+    // Data Services
     var covidService = CovidService();
     var countryService = CountryService();
     var selectedCountryData =  CountryData().countries;
@@ -26,11 +28,58 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupChart();
+        updateChartData();
+        
         self.covidService.delegate = self;
         self.countryPicker.delegate = self;
         self.countryPicker.dataSource = self;
     }
     
+    func setupChart(){
+        pieChartView.chartDescription?.text = ""
+        pieChartView.legend.enabled = false;
+        pieChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0, easingOption: ChartEasingOption.easeInBack)
+        numberOfDataEntries = [totalDeaths, totalInfected, totalRecovered];
+    }
+    
+    func updateChartData(){
+        let chartDataSet = PieChartDataSet(entries: numberOfDataEntries, label: nil);
+        let chartData = PieChartData(dataSet: chartDataSet);
+        
+        let colors = [UIColor.init(named: "deathCountColor"),UIColor.init(named: "recoveredCountColor"),UIColor.init(named: "infectedCountColor")];
+        
+        chartDataSet.colors = colors as! [NSUIColor];
+        pieChartView.data = chartData;
+    }
+}
+
+// MARK : CovidServiceDelegate
+
+extension ViewController: CovidServiceDelegate{
+    
+    func didUpdateCovidData(_ covidService: CovidService, covidData: CovidData){
+        let confirmed = covidData.result.last?.confirmed;
+        let recovered = covidData.result.last?.recovered;
+        let deaths = covidData.result.last?.deaths;
+        
+        self.totalInfected.value = Double(confirmed!);
+        self.totalRecovered.value = Double(recovered!);
+        self.totalDeaths.value = Double(deaths!);
+        
+        DispatchQueue.main.async {
+            self.updateChartData();
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
+
+// MARK - UIPickerDelegate
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -47,7 +96,12 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         let countryKey = Array(selectedCountryData)[row].keys;
         let countryName = Array(selectedCountryData)[row].values;
-        self.countryLabel.text = countryName.first!;
+        
+        
+        let myAttribute = [ NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-Light", size: 20.0)! ]
+        let myAttrString = NSAttributedString(string: countryName.first!, attributes: myAttribute)
+        
+        self.pieChartView.centerAttributedText = myAttrString;
         
         let date = Date();
         let formatter = DateFormatter();
@@ -56,42 +110,6 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         
         self.covidService.fetchData(countryCode: countryKey.first!, date: currentDate);
     }
-    
-    func didUpdateCovidData(_ covidService: CovidService, covidData: CovidData){
-        let confirmed = covidData.result.last?.confirmed;
-        let recovered = covidData.result.last?.recovered;
-        let deaths = covidData.result.last?.deaths;
-        
-        
-        if let deaths = deaths {
-            let result = String(deaths)
-            DispatchQueue.main.async {
-                self.totalDeathsCount.text = result;
-            }
-        }
-        
-        if let recovered = recovered {
-            let result = String(recovered)
-            DispatchQueue.main.async {
-                self.totalRecoveredCount.text = result;
-            }
-        }
-        
-        if let confirmed = confirmed {
-            let result = String(confirmed)
-            DispatchQueue.main.async {
-                self.totalInfectedCount.text = result;
-            }
-        }
-        
-        
-        //        self.totalDeathsCount.text = String (deaths);
-        //        self.totalInfectedCount.text = (covidData.result.last?.confirmed!);
-        //        self.totalRecoveredCount.text = covidData.result.last.recovered;
-    }
-    
-    func didFailWithError(error: Error) {
-        print(error)
-    }
 }
+
 
